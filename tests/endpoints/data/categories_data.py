e52db63ebdf6ec60469d950dc6146ad617fde6b4
Protages <k.stif@mail.ru>
2023-02-17 16:07:53 +0500

    Add tests and test data for categories enpoints

---
+++ tests/endpoints/data/categories_data.py
@@ -0,0 +1,177 @@
+'''Categories data for tests'''
+
+from tests.endpoints.data import user_data, games_data
+
+
+create_category_valid_data = [
+    {
+        'title': 'Category 1',
+        'user': 1
+    },
+    {
+        'title': 'Category 2',
+        'user': 1
+    },
+    {
+        'title': 'Category 3',
+        'user': 1
+    },
+    {
+        'title': 'Category 4',
+        'user': 1
+    },
+    {
+        'title': 'Category 5',
+        'user': 1
+    },
+]
+
+category_valid_data_response = [
+    {
+        'id': 1,
+        'title': 'Category 1',
+        'games': [],
+        'user': user_data.user_nested_response[0]
+    },
+    {
+        'id': 2,
+        'title': 'Category 2',
+        'games': [],
+        'user': user_data.user_nested_response[0]
+    },
+    {
+        'id': 3,
+        'title': 'Category 3',
+        'games': [],
+        'user': user_data.user_nested_response[0]
+    },
+    {
+        'id': 4,
+        'title': 'Category 4',
+        'games': [],
+        'user': user_data.user_nested_response[0]
+    },
+    {
+        'id': 5,
+        'title': 'Category 5',
+        'games': [],
+        'user': user_data.user_nested_response[0]
+    },
+]
+
+create_category_invalid_required_data = {
+    'bar': 'baz'  # required title, user
+}
+
+create_category_invalid_required_data_response = {
+    'detail': [
+        {
+            'loc': ['body', 'title'], 
+            'msg': 'field required', 
+            'type': 'value_error.missing'
+        }, 
+        {
+            'loc': ['body', 'user'], 
+            'msg': 'field required', 
+            'type': 'value_error.missing'
+        }
+    ]
+}
+
+create_category_invalid_user_id = {
+    'title': 'Category 999',
+    'user': 999
+}
+
+create_category_invalid_game_id = {
+    'title': 'Category 999',
+    'user': 1,
+    'games': [999, 1000]
+}
+
+create_category_invalid_unique = {
+    'title': 'Category 1',  # unique_together for title and user
+    'user': 1
+}
+
+category_invalid_unique_response = {
+    'detail': 'title and user field must be unique together'
+}
+
+category_invalid_id_response = {
+    'detail': 'Category does not exist'
+}
+
+update_category_valid_data = {
+    'title': 'Category 5 update'
+}
+
+update_category_valid_data_response = {
+    'id': 5,
+    'title': 'Category 5 update',
+    'games': [],
+    'user': user_data.user_nested_response[0]
+}
+
+update_category_invalid_unique = {
+    'title': 'Category 1',
+}
+
+update_category_invalid_required_data = {
+    'bar': 'baz'  # title required
+}
+
+update_category_invalid_required_data_response = {
+    'detail': [
+        {
+            'loc': ['body', 'title'], 
+            'msg': 'field required', 
+            'type': 'value_error.missing'
+        }
+    ]
+}
+
+add_games_to_category_valid_data = {
+    'games': [1, 2, 3, 4]
+}
+
+add_games_to_category_valid_data_response = {
+    'id': 5,
+    'title': 'Category 5 update',
+    'games': [
+        games_data.game_nested_response[0],
+        games_data.game_nested_response[1],
+        games_data.game_nested_response[2],
+        games_data.game_nested_response[3]
+    ],
+    'user': user_data.user_nested_response[0]
+}
+
+add_games_to_category_invalid_game_id = {
+    'games': [999]
+}
+
+remove_games_from_category_valid_data = {
+    'games': [4]
+}
+
+remove_games_from_category_valid_data_response = {
+    'id': 5,
+    'title': 'Category 5 update',
+    'games': [
+        games_data.game_nested_response[0],
+        games_data.game_nested_response[1],
+        games_data.game_nested_response[2]
+    ],
+    'user': user_data.user_nested_response[0]
+}
+
+remove_games_from_category_alredy_remove_response = {
+    'detail': 'Game with id 4 are not in category'
+}
+
+remove_games_from_category_invalid_game_id = {
+    'games': [999]
+}
+
+remove_all_games_from_category_response = update_category_valid_data_response
---
+++ tests/endpoints/test_8_categories.py
@@ -0,0 +1,203 @@
+# After all these tests, we have categories with id: 1, 2, 3, 4
+
+import pytest
+
+from fastapi.testclient import TestClient
+
+from tests.endpoints.data import categories_data, user_data, games_data
+
+
+@pytest.mark.parametrize(
+    'request_data, response_data',
+    [(a, b) for a, b in zip(
+        categories_data.create_category_valid_data,
+        categories_data.category_valid_data_response
+    )]
+)
+def test_create_category(test_client: TestClient, request_data, response_data):
+    response = test_client.post('/api/v1/category', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 201
+    assert response.json() == response_data
+
+
+def test_create_category_invalid_required_data(test_client: TestClient):
+    request_data = categories_data.create_category_invalid_required_data
+    response_data = categories_data.create_category_invalid_required_data_response
+    response = test_client.post('/api/v1/category', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 422
+    assert response.json() == response_data
+
+
+def test_create_category_invalid_user_id(test_client: TestClient):
+    request_data = categories_data.create_category_invalid_user_id
+    response_data = user_data.user_invalid_id_response
+    response = test_client.post('/api/v1/category', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_create_category_invalid_game_id(test_client: TestClient):
+    request_data = categories_data.create_category_invalid_game_id
+    response_data = games_data.game_invalid_id_response
+    response = test_client.post('/api/v1/category', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_create_category_invalid_unique(test_client: TestClient):
+    request_data = categories_data.create_category_invalid_unique
+    response_data = categories_data.category_invalid_unique_response
+    response = test_client.post('/api/v1/category', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_read_category(test_client: TestClient):
+    response_data = categories_data.category_valid_data_response[0]
+    response = test_client.get(f'/api/v1/category/{1}')
+    print('-----', response.json())
+    assert response.status_code == 200
+    assert response.json() == response_data
+
+
+def test_read_category_invalid_id(test_client: TestClient):
+    response_data = categories_data.category_invalid_id_response
+    response = test_client.get(f'/api/v1/category/{999}')
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_read_all_category(test_client: TestClient):
+    response_data = categories_data.category_valid_data_response
+    response = test_client.get(f'/api/v1/category/')
+    print('-----', response.json())
+    assert response.status_code == 200
+    assert response.json() == response_data
+
+
+@pytest.mark.parametrize(
+    'size, page, response_data',
+    [
+        (-10, 0, {'detail': 'The pagination size parameter must be >= 0'}),
+        (0, -10, {'detail': 'The pagination page parameter must be >= 0'}),
+    ]
+)
+def test_read_all_category_invalid_pagination(
+        test_client: TestClient, size, page, response_data
+    ):
+    response = test_client.get(f'/api/v1/category/?size={size}&page={page}')
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_update_category(test_client: TestClient):
+    request_data = categories_data.update_category_valid_data
+    response_data = categories_data.update_category_valid_data_response
+    response = test_client.put(f'/api/v1/category/{5}', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 200
+    assert response.json() == response_data
+
+
+def test_update_category_invalid_unique(test_client: TestClient):
+    request_data = categories_data.update_category_invalid_unique
+    response_data = categories_data.category_invalid_unique_response
+    response = test_client.put(f'/api/v1/category/{5}', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_update_category_invalid_required(test_client: TestClient):
+    request_data = categories_data.update_category_invalid_required_data
+    response_data = categories_data.update_category_invalid_required_data_response
+    response = test_client.put(f'/api/v1/category/{5}', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 422
+    assert response.json() == response_data
+
+
+def test_update_category_invalid_id(test_client: TestClient):
+    request_data = categories_data.update_category_valid_data
+    response_data = categories_data.category_invalid_id_response
+    response = test_client.put(f'/api/v1/category/{999}', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_add_games_to_category(test_client: TestClient):
+    request_data = categories_data.add_games_to_category_valid_data
+    response_data = categories_data.add_games_to_category_valid_data_response
+    response = test_client.post(f'/api/v1/category/{5}/games', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 200
+    assert response.json() == response_data
+
+
+def test_add_games_to_category_invalid_game_id(test_client: TestClient):
+    request_data = categories_data.add_games_to_category_invalid_game_id
+    response_data = games_data.game_invalid_id_response
+    response = test_client.post(f'/api/v1/category/{5}/games', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_remove_games_from_category(test_client: TestClient):
+    request_data = categories_data.remove_games_from_category_valid_data
+    response_data = categories_data.remove_games_from_category_valid_data_response
+    response = test_client.put(f'/api/v1/category/{5}/games', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 200
+    assert response.json() == response_data
+
+
+def test_remove_games_from_category_alredy_remove(test_client: TestClient):
+    request_data = categories_data.remove_games_from_category_valid_data
+    response_data = categories_data.remove_games_from_category_alredy_remove_response
+    response = test_client.put(f'/api/v1/category/{5}/games', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_remove_games_from_category_invalid_game_id(test_client: TestClient):
+    request_data = categories_data.remove_games_from_category_invalid_game_id
+    response_data = games_data.game_invalid_id_response
+    response = test_client.put(f'/api/v1/category/{5}/games', json=request_data)
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
+
+
+def test_remove_all_games_from_category(test_client: TestClient):
+    response_data = categories_data.remove_all_games_from_category_response
+    response = test_client.delete(f'/api/v1/category/{5}/games')
+    print('-----', response.json())
+    assert response.status_code == 200
+    assert response.json() == response_data
+
+
+def test_delete_category(test_client: TestClient):
+    response = test_client.delete(f'/api/v1/category/{5}')
+    assert response.status_code == 204
+
+    response = test_client.get(f'/api/v1/category/')
+    assert response.status_code == 200
+    assert len(response.json()) == 4
+
+
+def test_delete_category_invalid_id(test_client: TestClient):
+    response_data = categories_data.category_invalid_id_response
+    response = test_client.delete(f'/api/v1/category/{999}')
+    print('-----', response.json())
+    assert response.status_code == 400
+    assert response.json() == response_data
